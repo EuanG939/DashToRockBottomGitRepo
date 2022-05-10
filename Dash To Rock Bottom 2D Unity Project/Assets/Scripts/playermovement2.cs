e574edb0f4783936130de4925eedb29a0a6aa274
EuanG939 <78357958+EuanG939@users.noreply.github.com>
2022-05-10 12:56:24 +0100

    Completed level design for levels 2-5
    Sub-levels, building interiors and boss arena level design also finished

--- Dash To Rock Bottom 2D Unity Project/Assets/Scripts/playermovement2.cs
+++ Dash To Rock Bottom 2D Unity Project/Assets/Scripts/playermovement2.cs
@@ -8,7 +8,6 @@ public class playermovement2 : MonoBehaviour
     public CharacterController2D controller;
 
     public float runSpeed = 40f;
-
     float horizontalMove = 0f;
     bool jump = false;
     bool crouch = false;
@@ -16,17 +15,23 @@ public class playermovement2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //HorizontalMove is for getting left and right inputs which in this case is the keys 'A' and 'D'
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        if(Input.GetButtonDown("Jump"))
+        //Used for getting the jump function, which is the 'Space' key
+        if (Input.GetButtonDown("Jump"))
         {
             jump = true;
         }
-        
+
+        //Used for crouching, which is the 'S' key
         if (Input.GetButtonDown("Crouch"))
         {
             crouch = true;
         }
+
+        //If the player releases the crouch key, the player will no longer be crouched, this is to prevent the character being permanently crouched
         else if (Input.GetButtonUp("Crouch"))
         {
             crouch = false;
